87fef5b50dc505bc4355bd0f705cc7fab099e533
Jake Woratzeck <31188226+JakeWoratzeck@users.noreply.github.com>
2021-02-13 14:19:05 -0600

    Adjusted 404 section

--- solution.py
+++ solution.py
@@ -40,9 +40,9 @@ def webServer(port=13331):
            #Send response message for file not found (404)
            #Fill in start
            #send a 404 if the connection did NOT work
-           
+        
             connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", "UTF-8"))
-            connectionSocket.send(bytes("404 Not Found", "UTF-8"))
+            #connectionSocket.send(bytes("404 Not Found", "UTF-8"))
            #Fill in end
 
            #Close client socket
